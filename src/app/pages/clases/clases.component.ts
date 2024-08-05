23289870f63be013609b4995d053351628c9197e
hrizo-at-631462285142 <daymorrizzo@gmail.com>
2024-08-04 23:27:59 -0500

    info clases

--- src/app/pages/clases/clases.component.ts
+++ src/app/pages/clases/clases.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-clases",
@@ -6,6 +8,15 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./clases.component.scss"]
     })
 export class ClasesComponent implements OnInit {
-    constructor() { }
+    constructor(private router:Router,
+        private viewportScroller: ViewportScroller
+    ) { }
     ngOnInit() { }
+
+    navigateToInfoClases() {
+        console.log("Navigating to info clases");
+        this.router.navigate(['/clases/info-clases']).then(() => {
+            this.viewportScroller.scrollToPosition([0, 0]);
+          });
+    }
 }
