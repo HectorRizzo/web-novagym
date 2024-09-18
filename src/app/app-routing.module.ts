bdc867a920a9f18adbcc865f90a87fb1815ec359
hrizo-at-631462285142 <daymorrizzo@gmail.com>
2024-09-18 00:37:56 -0500

    usehash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
 export const routes: Routes = [
   { path: "", loadChildren: () => import("./pages/pages.module").then((m) => m.PagesModule) },
+  {path:"noticias2", component: NoticiasComponent},
   //tienda
   { path: "shop", loadChildren: () => import("./pages/shop/shop.module").then((m) => m.ShopModule) },
   {path: "**", redirectTo: "/home", pathMatch: "full"}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { PagesModule } from './pages/pages.module';
 import { ShopModule } from './pages/shop/shop.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { ShopModule } from './pages/shop/shop.module';
     ShopModule
   ],
   providers: [
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 })
 export class AppModule { }
